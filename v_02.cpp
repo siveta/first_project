eca760b9f43a116a5c8e0317f63246c74b0f579c
siveta <71584208+siveta@users.noreply.github.com>
2020-09-26 15:19:00 +0300

    Add files via upload

---
+++ v_02.cpp
@@ -0,0 +1,58 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+
+struct studentas {
+    string vardas;
+    string pavarde;
+    int egz;
+    int paz[5];
+    float galutinis = 0;
+};
+
+int main() {
+    std::ofstream file("kursiokai.txt");
+    int a = 0;
+    vector <studentas> grupe;
+    studentas laikina;
+    cout << "Sveiki !\n ";
+    cout << "Iveskite studentu skaiciu: ";
+    cin >> a;
+    grupe.reserve(a);
+    for (int i = 0; i < a; i++) {
+        cout << "Iveskite " << i + 1 << "-ojo studento varda: ";
+        cin >> laikina.vardas;
+        cout << "Iveskite " << i + 1 << "-ojo studento pavarde: ";
+        cin >> laikina.pavarde;
+        cout << "Iveskite " << i + 1 << "-ojo studento egzamino rezultata: ";
+        cin >> laikina.egz;
+        for (int j = 0; j < 5; j++) {
+            cout << "Iveskite " << j + 1 << "-aji pazymi :\n";
+            cin >> laikina.paz[j];
+            if (laikina.paz[j] < 0 or laikina.paz[j] > 10) {
+                cout << "Klaida! Iveskite pazymius dar karta:\n";
+                j = j - 1;
+            }
+            else {
+                laikina.galutinis = laikina.galutinis + (float)laikina.paz[j];
+            }
+        }
+        laikina.galutinis = laikina.galutinis / 5;
+        laikina.galutinis = laikina.galutinis * 0.4 + 0.6 * laikina.egz;
+
+        grupe.push_back(laikina);
+    }
+    file << "Pavarde\t\t" << "Vardas\t\t" << "Galutinis(Vid.)" << endl;
+    file << "------------------------------------------------------------------------\n";
+    for (auto& tt : grupe)
+        file << tt.vardas << "\t\t" << tt.pavarde << "\t\t" << tt.galutinis << endl;
+    grupe.clear();
+}
