532e027ab24dbfeb23797bdb17b7c22c3a1c4cf9
siveta <71584208+siveta@users.noreply.github.com>
2020-09-26 11:35:55 +0300

    Add files via upload

--- v_01.cpp
+++ v_01.cpp
@@ -16,30 +16,33 @@ struct studentas {
     float galutinis = 0;
 };
 
-int main()
-{
+int main() {
     int n = 0;
-    studentas Eil;
-    cout << "Sveiki, iveskite eil.duomenis (vardas, pavarde, egz.paz.):\n";
-    cin >> Eil.vardas >> Eil.pavarde >> Eil.egz;
-    cout << "Iveskite skaiciu kiek namu darbu noresite ivesti :";
+    cout << "Sveiki!\n";
+    cout << "Irasykite studentu skaiciu :\n";
     cin >> n;
-
+    studentas* grupe = new studentas[n];
     for (int i = 0; i < n; i++) {
-        cout << "Iveskite " << i + 1 << "-aji pazymi :\n";
-        cin >> Eil.paz[i];
-        if (Eil.paz[i] <= 0 or Eil.paz[i] > 10) {
-            cout << "Klaida! Iveskite pazymius dar karta:\n";
-            i = i - 1;
-        }
-        else {
-            Eil.galutinis = Eil.galutinis + (float)Eil.paz[i];
-        }
+        cout << "Iveskite eil.duomenis (vardas, pavarde, egz.paz.):\n";
+        cin >> grupe[i].vardas >> grupe[i].pavarde >> grupe[i].egz;
+        for (int j = 0; j < 5; j++) {
+            cout << "Iveskite " << j + 1 << "-aji pazymi :\n";
+            cin >> grupe[i].paz[j];
+            if (grupe[i].paz[j] < 0 or grupe[i].paz[j] > 10 or isdigit(grupe[i].paz[j] = false)) {
+                cout << "Klaida! Iveskite pazymius dar karta:\n";
+                j = j - 1;
+            }
+            else {
+                grupe[i].galutinis = grupe[i].galutinis + (float)grupe[i].paz[j];
+            }
 
+        }
+        grupe[i].galutinis = grupe[i].galutinis / 5;
+        grupe[i].galutinis = grupe[i].galutinis * 0.4 + 0.6 * grupe[i].egz;
     }
-    Eil.galutinis = Eil.galutinis / n;
-    Eil.galutinis = Eil.galutinis * 0.4 + 0.6 * Eil.egz;
-
-    cout << "Pavarde\t\t" << "Vardas\t\t" << "Galutinis(Vid.)\n";
+    cout << "Pavarde\t\t" << "Vardas\t\t" << "Galutinis(Vid.)" << "Galutinis(Med.)\n";
     cout << "------------------------------------------------------------------------\n";
-    cout << Eil.vardas << "\t\t" << Eil.pavarde << "\t\t" << Eil.galutinis;
+    for (int i = 0; i < n; i++) {
+        cout << grupe[i].vardas << "\t\t" << grupe[i].pavarde << "\t\t" << grupe[i].galutinis << endl;
+    }
+}
