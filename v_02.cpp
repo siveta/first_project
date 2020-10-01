433c9bc3f0b35ff545bd7fb0de8020c701344a0e
siveta <71584208+siveta@users.noreply.github.com>
2020-10-01 20:58:30 +0300

    Add files via upload

--- v_02.cpp
+++ v_02.cpp
@@ -4,8 +4,6 @@
 #include <vector>
 #include <string>
 #include <fstream>
-#include <stdio.h> 
-#include <conio.h>
 #include <iomanip>
 
 using std::string;
@@ -18,20 +16,21 @@ struct studentas {
     string vardas;
     string pavarde;
     int egz;
-    int paz[20];
     float med;
     float galutinis = 0;
 };
 
 int main() {
+
     int m = 0;
     int a = 0;
-    int choice;
+    int paz;
+    int choice = 0;
     vector <studentas> grupe;
     studentas laikina;
     vector <int> pazymiai;
     cout << "Sveiki !\n ";
-    cout << "Pasirinkite operacija :\n1. Irasyti studentu duomenis i duomenu baze.\n2. Nuskaityti duomenis is failo\n";
+    cout << "Pasirinkite operacija (irasykite skaiciu):\n1. Irasyti studentu duomenis i duomenu baze.\n2. Nuskaityti duomenis is failo\n";
     cin >> choice;
     if (choice == 1) {
         std::ofstream file("kursiokai.txt");
@@ -39,15 +38,8 @@ int main() {
         cin >> a;
         grupe.reserve(a);
         file << "Vardas\t" << "Pavarde\t";
-        cout << "Kiek pazymiu noresite ivesti (maksimalus galimas ivedimas - 15 pazymiu)?\n";
-        // patikrina ar norima ivesti ne maziau 0 ir ne daugiau  kaip 15 pazymiu
-        while (true) {
-            cin >> m;
-            if (m < 1 or 20 < m) {
-                cout << "KLAIDA! negalimas veiksmas\n";
-            }
-            else { break; }
-        }
+        cout << "Kiek pazymiu noresite ivesti?\n";
+        cin >> m;
         pazymiai.reserve(m);
         for (int i = 0; i < m; i++) {
             file << "ND" << i + 1 << "\t";
@@ -62,25 +54,25 @@ int main() {
             file << laikina.pavarde << "\t";
             cout << "Iveskite " << i + 1 << "-ojo studento egzamino rezultata: ";
             cin >> laikina.egz;
-            
+
             for (int j = 0; j < m; j++) {
                 cout << "Iveskite " << j + 1 << "-aji pazymi :\n";
-                cin >> laikina.paz[j];
-                // patikrina ar ivestas pazymys yra  10-baleje sistemoje
-                if (laikina.paz[j] < 0 or laikina.paz[j] > 10) {
+                cin >> paz;
+                // patikrina ar ivestas pazymys yra 10-baleje sistemoje
+                if (paz < 0 or paz > 10) {
                     cout << "Klaida! Iveskite pazymius dar karta:\n";
                     j = j - 1;
                 }
                 else {
-                    file << laikina.paz[j] << "\t";
-                    laikina.galutinis = laikina.galutinis + (float)laikina.paz[j];
-                    pazymiai.push_back(laikina.paz[j]);
+                    file << paz << "\t";
+                    laikina.galutinis = laikina.galutinis + (float)paz;
+                    pazymiai.push_back(paz);
                 }
                 // mediana
                 std::nth_element(pazymiai.begin(), pazymiai.begin() + pazymiai.size() / 2, pazymiai.end());
                 laikina.med = pazymiai[pazymiai.size() / 2];
+                pazymiai.clear();
             }
-
             laikina.galutinis = laikina.galutinis / m;
             laikina.galutinis = laikina.galutinis * 0.4 + 0.6 * laikina.egz;
             file << laikina.egz << endl;
@@ -88,13 +80,39 @@ int main() {
         }
     }
     else if (choice == 2) {
+        int lines = 0;
         string name;
+        int nd;
+        int word = 0;
+        int size = 0;
+        string words;
+        string line;
+        std::ifstream myfile;
         cout << "Iveskite failo, kuri norite nuskaityti, pavadinima:\n";
         cin >> name;
-        std::ifstream file2(name);
+        myfile.open(name);
+        if (myfile.is_open()) {
+            cout << "failas atidarytas!" << endl;
+            string l;
+            getline(myfile, l); // pirma eilute
+            cout << l << endl;
+            string temp = l;
+            
+            while (myfile >> line) {
+                getline(myfile, line);
+                lines++; // studentu skaicius bus lygus eiluciu skaiciui - 1
+            }
+        }
+        else {
+            cout << "Negalima atidaryti failo!" <<endl;
+        }
 
-    
-    } 
+        cout << "zodziai : " << word << endl;
+        cout << "eilutes : " << lines << endl;
+        nd = (word / lines) - 3;
+        cout << "nd : " << nd << endl;
+        myfile.close();
+    }
 
     cout << "Vardas\t\t" << "Pavarde\t\t" << "Galutinis(Vid.)" << "Galutinis(Med.)" << endl;
     cout << "------------------------------------------------------------------------\n";
