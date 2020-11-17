f3a7aeab21291fa9570e462dc706b343ad6acbf9
siveta <71584208+siveta@users.noreply.github.com>
2020-11-17 20:09:25 +0200

    Add files via upload

--- v_05.cpp
+++ v_05.cpp
@@ -21,22 +21,22 @@ int main() {
 
     cout << "Failo 'studentai2.txt' nuskaitymas :\t(failas is 10000 eiluciu)" << endl;
     greicio_analize_List("studentai2.txt", m);
-    greicio_analize_Vector("studentai1.txt", m, 10000);
+    greicio_analize_Vector("studentai2.txt", m, 10000);
     cout << "----------------------------------------------------------------------" << endl;
 
     cout << "Failo 'studentai3.txt' nuskaitymas :\t(failas is 100000 eiluciu)" << endl;
     greicio_analize_List("studentai3.txt", m);
-    greicio_analize_Vector("studentai1.txt", m, 100000);
+    greicio_analize_Vector("studentai3.txt", m, 100000);
     cout << "----------------------------------------------------------------------" << endl;
 
     cout << "Failo 'studentai4.txt' nuskaitymas :\t(failas is 1000000 eiluciu)" << endl;
     greicio_analize_List("studentai4.txt", m);
-    greicio_analize_Vector("studentai1.txt", m, 1000000);
+    greicio_analize_Vector("studentai4.txt", m, 1000000);
     cout << "----------------------------------------------------------------------" << endl;
 
     cout << "Failo 'studentai5.txt' nuskaitymas :\t(failas is 10000000 eiluciu)" << endl;
     greicio_analize_List("studentai5.txt", m);
-    greicio_analize_Vector("studentai1.txt", m, 10000000);
+    greicio_analize_Vector("studentai5.txt", m, 10000000);
     cout << "----------------------------------------------------------------------" << endl;
     
 }
